5704b7e4fb55215453909ea2f30b143a4cc81d20
BiFr0st <95887189+B1Fr0st@users.noreply.github.com>
2022-01-12 18:04:01 +0000

    Removed obsolete dependencies.

--- PixScanner.py
+++ PixScanner.py
@@ -2,12 +2,11 @@ from PIL import Image
 import time
 import pyperclip
 from os import remove
-#the only modules required are pyperclip and Pillow.
+#the only modules required are pyperclip and Pillow
 #currently, 400x400 corrupts on row 27. [Reproduced twice.]
 #TODO:Make the user experience more friendly(Make url an input at beginning?)FINISHED
 #TODO:Add way to copy raw data while using replit.
-from bs4 import BeautifulSoup
-import requests,urllib.request,shutil
+import requests,shutil
 
 
 
@@ -56,64 +55,76 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 
 def ReturnColors(img_path,pixSize,pSize):
 
-    print("Scanning all pixels to grab all possible colors.")
-    img = Image.open(img_path).convert('RGBA')
-    width,height = img.size
-    colors = {}
-    row = pixSize/2
-    pixel = pixSize/2
-    bitmap = []
-    pushArray = ""
-    printProgressBar(0,width,prefix="Scanning:",suffix="Complete",length = 50)
-    start = time.time()
-    while row < height:
-        while pixel < width:
-            pixCol = get_pixel(img_path,pixel,row)
-            if pixCol in colors:
-                pass
-            elif pixCol not in colors:
-                colors[pixCol] = str(len(colors))
-            bit = colors[pixCol]
-            pushArray += bit
-            pixel += pixSize
-        pixel = 0
-        if len(pushArray) == pSize:
-            row += pixSize
-            bitmap.append(pushArray)
-        else:
-          print("Image corruption on row %i.\n" % ((row-pixSize/2)/pixSize))
+  print("Scanning all pixels to grab all possible colors.")
+  img = Image.open(img_path).convert('RGBA')
+  width,height = img.size
+  colors = {}
+  row = pixSize/2
+  pixel = pixSize/2
+  bitmap = []
+  pushArray = ""
+  printProgressBar(0,width,prefix="Scanning:",suffix="Complete",length = 50)
+  start = time.time()
+  while row < height:
+      while pixel < width:
+          pixCol = get_pixel(img_path,pixel,row)
+          if pixCol in colors:
+              pass
+          elif pixCol not in colors:
+              colors[pixCol] = str(len(colors))
+          bit = colors[pixCol]
+          pushArray += bit
+          pixel += pixSize
+      pixel = 0
+      if len(pushArray) == pSize:
+          row += pixSize
+          bitmap.append(pushArray)
+      else:
+        print("Image corruption on row %i.\n" % ((row-pixSize/2)/pixSize))
+
+      pushArray = ""
+      printProgressBar((row-pixSize/2),width,prefix="Scanning:",suffix="Complete",length = 50)
+  end = time.time()
+  elapsed = end-start
+  use_map = {v:k for k,v in colors.items()}
+  #final string making a comeback
+  finalString = "var scene = {\nart:\n"
+  finalString += str(bitmap)+",\n"
+  finalString += "palette:{"
+  #here is going to be the code for making the colors not need adding "color", and if they do, it'll make it easier
+  for item in use_map.items():
+      itemm = item
+      #so, what we want to do is a few things.
+      #1:Add in a newline character between each "item"
+      #2:Add in a "color" before each set of RGB items.
+      #we can accomplish 1 and 2 with this code:
+      item = "" #defines the item as a string
+      item += "'"+itemm[0]+"':" # adds in the key
+      item += "color"+str(itemm[1]) #adds in the value.
+      item += ","
+      finalString += item+"\n"
+  finalString += "},\n"
+  finalString += "pixSize:"+str(pixSize)
+  finalString += "\n};"
+  print("Total time taken to compile image:"+str(elapsed))
+  copier = input("Copy data to clipboard? [Y\\N]")
+  if copier == "N":
+    print(finalString)
+
+  elif copier == "Y":
+	  pyperclip.copy(finalString)
+
+		
 
 
-
-        pushArray = ""
-        printProgressBar((row-pixSize/2),width,prefix="Scanning:",suffix="Complete",length = 50)
-    end = time.time()
-    elapsed = end-start
-    use_map = {v:k for k,v in colors.items()}
-    #final string making a comeback
-    finalString = "var scene = {\nart:\n"
-    finalString += str(bitmap)+",\n"
-    finalString += "palette:{"
-    #here is going to be the code for making the colors not need adding "color", and if they do, it'll make it easier
-    for item in use_map.items():
-        itemm = item
-        #so, what we want to do is a few things.
-        #1:Add in a newline character between each "item"
-        #2:Add in a "color" before each set of RGB items.
-        #we can accomplish 1 and 2 with this code:
-        item = "" #defines the item as a string
-        item += "'"+itemm[0]+"':" # adds in the key
-        item += "color"+str(itemm[1]) #adds in the value.
-        item += ","
-        finalString += item+"\n"
-    finalString += "},\n"
-    finalString += "pixSize:"+str(pixSize)
-    finalString += "\n};"
-    copier = input("Copy data to clipboard? [Y\\N]")
-    if copier == "Y":
-      pyperclip.copy(finalString)
-      print("Data copied to clipboard.")
-
+oss = input("Are you using a windows installed code runner? (no online IDES) [Y\\N]")
+if oss == "N":
+	exit()
+elif oss == "Y":
+	pass
+else:
+	print("Invalid response.")
+	exit()
 isUrl = input("Did you download the image already or are you going to use a url? [True/False; True for downloaded]")
 deleteImg = input("Would you like to delete the image after it is converted?[True\\False]")
 if isUrl == "True":
