c107e2b81b1b5fa8c879a3eac4498c0e058b33e0
BiFr0st <95887189+B1Fr0st@users.noreply.github.com>
2022-01-14 21:14:47 +0000

    Efficiency

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from PIL import Image
 import time
 import pyperclip
 from os import remove
+from functools import cache
 #the only modules required are pyperclip and Pillow
 #currently, 400x400 corrupts on row 27. [Reproduced twice.]
+<<<<<<< HEAD:main.py
 #TODO:Make the user experience more friendly(Make url an input at beginning?)FINISHED
 #TODO:Add way to copy raw data while using replit.https://art.pixilart.com/c78901132398980.png
 import requests,shutil
@@ -18,9 +20,25 @@ def download_image(image,name):
 	response.raw.decode_content = True
 	shutil.copyfileobj(response.raw,file)
 	del response
+=======
+#TODO:Make the user experience more friendly(Make url an input at beginning?)FINISHED
+#TODO:Add way to copy raw data while using replit.
+import requests,shutil
+>>>>>>> origin/main:PixScanner.py
 
 
 
+
+def download_image(image,name):
+	response = requests.get(image,stream=True)
+	file = open("./"+name,"wb")
+
+	response.raw.decode_content = True
+	shutil.copyfileobj(response.raw,file)
+	del response
+
+
+@cache
 def get_pixel(img_path,x,y):
   im = Image.open(img_path).convert('RGBA')
     
@@ -65,6 +83,7 @@ def ReturnColors(img_path,pixSize,pSize):
   printProgressBar(0,width,prefix="Scanning:",suffix="Complete",length = 50)
   start = time.time()
   while row < height:
+<<<<<<< HEAD:main.py
     while pixel < width:
 			
       r,g,b,a = img.getpixel((pixel,row))
@@ -84,6 +103,27 @@ def ReturnColors(img_path,pixSize,pSize):
     pushArray = ""
       
     printProgressBar((row-pixSize/2),width,prefix="Scanning:",suffix="Complete",length = 50)
+=======
+      
+      while pixel < width:
+          pixCol = get_pixel(img_path,pixel,row)
+          if pixCol in colors:
+              pass
+          elif pixCol not in colors:
+              colors[pixCol] = str(len(colors))
+          bit = colors[pixCol]
+          pushArray += bit
+          pixel += pixSize
+      pixel = 0
+      if len(pushArray) == pSize:
+          row += pixSize
+          bitmap.append(pushArray)
+      else:
+        print("Image corruption on row %i.\n" % ((row-pixSize/2)/pixSize))
+
+      pushArray = ""
+      printProgressBar((row-pixSize/2),width,prefix="Scanning:",suffix="Complete",length = 50)
+>>>>>>> origin/main:PixScanner.py
   end = time.time()
   elapsed = end-start
   use_map = {v:k for k,v in colors.items()}
@@ -134,6 +174,17 @@ else:
 	download_image(url,"PixelArt.png")
 pxSize = int(input("Pixel size specified on Pixilart:"))
 imgPath = "PixelArt.png"
+<<<<<<< HEAD:main.py
+=======
+start = time.time()
+for i in range(0,pxSize):
+	get_pixel(imgPath,0,0)
+
+finish = time.time()
+elapse = finish-start
+print("Time taken to read %i pixels:%i seconds"%(pxSize,elapse))
+print("Estimated image conversion time:%i minutes"%(elapse*pxSize/60))
+>>>>>>> origin/main:PixScanner.py
 imgg = Image.open(imgPath).convert("RGB")
 width,height = imgg.size
 pixSize = width/pxSize
